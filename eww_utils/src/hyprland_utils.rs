aa485086d8ab581432c60bd902f123cd0a840557
DTheLegend <me@daudi.dev>
2024-09-29 21:27:01 +0100

    Fixed to work with latest hyprland

--- eww_utils/src/hyprland_utils.rs
+++ eww_utils/src/hyprland_utils.rs
@@ -19,6 +19,7 @@ pub struct HyprctlActiveWindowObject {
     pub size: [u32; 2],
     pub workspace: HyprctlShortWorkspaceObject,
     pub floating: bool,
+    pub pseudo: bool,
     pub monitor: i32,
     pub class: String,
     pub title: String,
@@ -27,11 +28,11 @@ pub struct HyprctlActiveWindowObject {
     pub pid: u32,
     pub xwayland: bool,
     pub pinned: bool,
-    pub fullscreen: bool,
-    pub fullscreen_mode: i32,
-    pub fake_fullscreen: bool,
+    pub fullscreen: i32,
+    pub fullscreen_client: i32,
     pub grouped: Vec<String>,
-    pub swallowing: String
+    pub swallowing: String,
+    pub focus_history_i_d: i32
 }
 
 #[derive(Deserialize)]
@@ -83,20 +84,26 @@ pub fn get_hyprland_events() -> Result<impl Iterator<Item=Result<HyprlandEvent, 
                 VarError::NotPresent => "No HYPRLAND_INSTANCE_SIGNATURE detected!".to_owned(),
                 VarError::NotUnicode(string) => format!("HYPRLAND_INSTANCE_SIGNATURE is not Unicode? Got: \"{:?}\"", string)
             }
-        });
+        })?;
+
+    let xdg_runtime_dir = var("XDG_RUNTIME_DIR")
+        .map_err(|err| {
+            match err {
+                VarError::NotPresent => "No XDG_RUNTIME_DIR detected!".to_owned(),
+                VarError::NotUnicode(string) => format!("XDG_RUNTIME_DIR is not Unicode? Got: \"{:?}\"", string)
+            }
+        })?;
     
     let re = match Regex::new(r"(?P<EVENT>\w+)>>(?P<DATA>.+)") {
         Ok(re) => re,
         Err(_) => return Err("Failed to compile regex!".to_owned())
     };
 
-    let socket_address = hyprland_instance_signature.map(|sig| format!("/tmp/hypr/{}/.socket2.sock", sig));
+    let socket_address = format!("{xdg_runtime_dir}/hypr/{hyprland_instance_signature}/.socket2.sock");
 
-    let buffered_connection = socket_address.and_then(
-        |addr| UnixStream::connect(&addr)
-            .map_err(|err| format!("Failed to connect to Unix Socket at \"{}\"!\nReason: {:?}", addr, err.kind()))
-            .map(BufReader::new)
-    );
+    let buffered_connection = UnixStream::connect(&socket_address)
+            .map_err(|err| format!("Failed to connect to Unix Socket at \"{}\"!\nReason: {:?}", socket_address, err.kind()))
+            .map(BufReader::new);
 
     buffered_connection.map( |b| {
         b.lines().map(move |line_result| {
